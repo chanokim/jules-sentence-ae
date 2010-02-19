4391d5a767980d41b17769a28643505e3cc2c932
tomanek <tomanek@7ac257a9-6a28-0410-b245-f828558649ed>
2010-02-19 08:03:15 +0000

    can produce standalone JSBD package now
    git-svn-id: https://www.coling.uni-jena.de/svn/jules/trunk/jules-sentence-ae@6399 7ac257a9-6a28-0410-b245-f828558649ed

--- makeJSBDtgz.sh
+++ makeJSBDtgz.sh
@@ -1,5 +1,8 @@
 ## uncomment the build part in pom.xml
 
+## before creating a tar-ball for distribution to third party, you might want to add some
+## trained models (if so, add then into directory models)
+
 # make binaries
 mvn clean
 mvn dependency:copy-dependencies
@@ -9,22 +12,21 @@ rm lib/*
 cp target/JSBD-2.4.jar lib/.
 cp target/dependency/*.jar lib/.
 rm lib/uima*.jar
-rm lib/jules-*.jar
 
 # make documentation
 cd doc
-pdflatex UIMA-JSBD.tex
-pdflatex UIMA-JSBD.tex
-bibtex UIMA-JSBD
-pdflatex UIMA-JSBD.tex
-cp UIMA-JSBD.pdf ../UIMA-JSBD-2.4.pdf
+pdflatex JSBD.tex
+pdflatex JSBD.tex
+bibtex JSBD
+pdflatex JSBD.tex
+cp JSBD.pdf ../JSBD-2.4.pdf
 cd ..
 
 
 # do the packaging
-tar -czvf JSBD-2.4.tgz src lib testdata models \
-JSBD-2.4.jar README LICENSE COPYRIGHT UIMA-JSBD-2.4.pdf \
---exclude=".svn" --exclude "*jules*" --exclude "*src/test*" --exclude "julie*types.xml"
+tar -czvf JSBD-2.4.tgz runJSBDpackaged.sh src lib testdata models \
+README LICENSE COPYRIGHT JSBD-2.4.pdf \
+--exclude=".svn" --exclude "*jules*java" --exclude "*src/test*" --exclude "julie*types.xml"
 
-rm UIMA-JSBD.pdf
+rm JSBD-2.4.pdf
 
