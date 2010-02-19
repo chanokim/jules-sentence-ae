e48bfc9307949a5c9e0633e369d4c23ba9038f73
tomanek <tomanek@7ac257a9-6a28-0410-b245-f828558649ed>
2010-02-19 08:35:26 +0000

    git-svn-id: https://www.coling.uni-jena.de/svn/jules/trunk/jules-sentence-ae@6404 7ac257a9-6a28-0410-b245-f828558649ed

--- scripts/runJSBD.sh
+++
@@ -1,9 +0,0 @@
-
-source setCP.sh
-
-#java -XX:+PrintGCDetails \
-#-verbose:gc \
-#-XX:+PrintGCTimeStamps \
-#-Xmx2000m -cp $CLASSPATH de.julielab.jsbd.SentenceSplitterApplication $*
-
-java -Xmx2000m -cp $CLASSPATH de.julielab.jsbd.SentenceSplitterApplication $*
